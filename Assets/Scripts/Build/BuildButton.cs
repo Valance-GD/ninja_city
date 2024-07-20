c70d655683f3ef4a331ddc8ee20ea69935e4c474
Valance <anton092330@gmail.com>
2024-07-20 19:07:28 +0300

    all

--- Assets/Scripts/Build/BuildButton.cs
+++ Assets/Scripts/Build/BuildButton.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildButton : MonoBehaviour
@@ -7,9 +5,9 @@ public class BuildButton : MonoBehaviour
     [SerializeField] private GameObject _building;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent(out PlayerController player))
+        if (other.TryGetComponent(out PlayerResurses player))
         {
-            if (player.GetComponent<PlayerResurses>().SpendResource("Wood", 20))
+            if(player.SpendResource("Wood", 20))
             {
                 _building.SetActive(true);
                 Destroy(gameObject);
