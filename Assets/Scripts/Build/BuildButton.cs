f4c9d8b43ddf3694841912faaf049c968ec4dda7
Valance <anton092330@gmail.com>
2024-07-29 19:10:34 +0300

    add effects

--- Assets/Scripts/Build/BuildButton.cs
+++ Assets/Scripts/Build/BuildButton.cs
@@ -1,21 +1,57 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class BuildButton : MonoBehaviour
 {
     [SerializeField] private GameObject _building;
     [SerializeField] private int _resoursAmountToBuild;
-    [SerializeField] protected string _resoursTypeToBuild;
+     private int _resoursAmountDef;
+    [SerializeField] private string _resoursTypeToBuild;
+    [SerializeField] private TextMeshProUGUI _leftAmountText;
+    [SerializeField] private GameObject _spendEffect;
+    private void Start()
+    {
+        _resoursAmountDef = _resoursAmountToBuild;
+        _leftAmountText.text = _resoursAmountToBuild.ToString();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out PlayerController player))
         {
-
-            if (ResurcesManager.Instance.SpendResource(_resoursTypeToBuild, _resoursAmountToBuild))
+            StartCoroutine(SpendMoneyWithDelay());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerController player))
+        {
+            StopAllCoroutines();
+        }
+    }
+    private IEnumerator SpendMoneyWithDelay()
+    {
+        for (int i = 0; i < _resoursAmountDef; i++)
+        {
+            if (ResurcesManager.Instance.SpendResource(_resoursTypeToBuild, 1))
             {
-                BuildHouse();
-                GameController.Instance.Save();
+                _spendEffect.SetActive(true);
+                _resoursAmountToBuild--;
+                _leftAmountText.text = _resoursAmountToBuild.ToString();
+                if (_resoursAmountToBuild == 0)
+                {
+                    BuildHouse();
+                    GameController.Instance.Save();
+                    _spendEffect.SetActive(false);
+                    break;
+                }
             }
-
+            else
+            {
+                _spendEffect.SetActive(false);
+                break;
+            }
+            yield return new WaitForFixedUpdate();
         }
     }
     public void BuildHouse()
